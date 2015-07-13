ca85a9e9c8f05e56b2fa3f80283a2d2e15cd8088
Apone <soyapone@gmail.com>
2015-07-13 17:40:28 +0200

    Adaptado a Express-partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //var users = require('./routes/user');
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 //app.use(app.router);
 app.use('/',routes);
 
