1668218d2a8900b572bd8d31c159032422e64db7
Apone <soyapone@gmail.com>
2015-07-13 14:32:55 +0200

    Esqueleto Modificaciones

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes');
-var users = require('./routes/user');
+var routes = require('./routes/index');
+//var users = require('./routes/user');
 
 var app = express();
 
@@ -21,10 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+//app.use(app.router);
+app.use('/',routes);
 
-app.get('/', routes.index);
-app.get('/users', users.list);
+//app.get('/', routes.index);
+//app.get('/users', users.list);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 /* GET home page. */
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res){
+  res.render('index', {title:'Quiz'});
+});
+
+module.exports = router;
