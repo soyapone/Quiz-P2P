8bd2762cf6de464fe7563f1bb7123afc97798016
Apone <soyapone@gmail.com>
2015-07-13 15:37:42 +0200

    Mi primera pregunta

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname,'/public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(app.router);
@@ -34,12 +34,13 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err
@@ -50,6 +51,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
