ddfab4d704a1c3302dd956b89e8d799f7d036458
Apone <soyapone@gmail.com>
2015-09-14 12:35:35 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Página de entrada. La home page */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
   res.render('author');
