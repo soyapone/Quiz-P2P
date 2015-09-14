556dbb864dfc34c5c7a4cdab2c08e40b301b9e50
Apone <soyapone@gmail.com>
2015-09-14 15:18:16 +0200

    Revert "Autoload (o preload)"
    This reverts commit 71adb165fd27d5c115a98e69cbd248fdcca0a885.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,36 +1,28 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
-    function(quiz) {
-      if (quiz) {
-        req.quiz = quiz;
-        next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
-    }
-  ).catch(function(error) { next(error);});
-};
-
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
+  })
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', { quiz: quiz});
+  })
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  var resultado = 'Incorrecto';
-  if (req.query.respuesta === req.quiz.respuesta) {
-    resultado = 'Correcto';
-  }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer',
+                 { quiz: quiz, respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer',
+                 { quiz: quiz, respuesta: 'Incorrecto'});
+    }
+  })
 };
